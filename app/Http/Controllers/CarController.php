95bfb0628aa61049f4c92c5b050deb090303479f
illia <illia.kyzlaitis.cv@gmail.com>
2017-01-01 18:14:00 +0200

    докатились, установка всякого говна занимает 1 час

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Models\Car;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class CarController extends Controller
+{
+
+    public function index() {
+        $cars = Car::orderBy('created_at', 'desc')->get();
+
+
+        return view('car.index', compact($cars));
+    }
+
+}
